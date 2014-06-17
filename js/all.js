598ada652d1c105286b68d37d25ec75322941e98
Dave Santos <info@davesantos.com>
2014-06-17 13:18:03 -0700

    Fixed close expanded on search

--- js/all.js
+++ js/all.js
@@ -1,7 +1,7 @@
 var search = document.getElementById('s');
 var list = document.getElementById('itemlist');
 var listItems = list.querySelectorAll('li');
-
+var expanded = list.querySelectorAll('.info');
 // search.focus();
 
 search.addEventListener('keyup', function(e) {
@@ -10,11 +10,14 @@ search.addEventListener('keyup', function(e) {
 	if( e.target.value.length > 0) {
 	  var text = listItems[i].innerHTML;
 
-	  if( !text.match(val)) {
-		listItems[i].classList.add('is-hidden');
-	  } else {
-		listItems[i].classList.remove('is-hidden');
-	  }
+		if( !text.match(val)) {
+			listItems[i].classList.add('is-hidden');
+			expanded[i].classList.add('is-hidden');
+		} else {
+			listItems[i].classList.remove('is-hidden');
+			expanded[i].classList.add('is-hidden');
+		}
+
 	} else {
 	  listItems[i].classList.remove('is-hidden');
 	}
@@ -23,12 +26,22 @@ search.addEventListener('keyup', function(e) {
 });
 
 
+$(function(){
+    $('#itemlist:[unselectable=on]').mousedown(function(event) {
+        event.preventDefault();
+        return false;
+    });
+});
+
+
 
 var item = $('.item');
 var allItems = $('#itemlist > .info');
 
 item.on('click', function() {
-	$(this).find('.info').toggleClass('is-hidden');
+	var t = $(this);
+	t.addClass('active');
+	t.find('.info').toggleClass('is-hidden');
 	return false;
 });
 
